76791e8a996206be50bf9467087b49270291cb53
sherif salama <136256923+Sherifsalam@users.noreply.github.com>
2026-06-29 05:43:46 +0300

    authentication validation

---
+++ src/Modules/auth/auth.validation.js
@@ -0,0 +1,16 @@
+import joi from"joi"
+import { Gender } from "../../DB/enums/user.enums.js"
+
+
+
+
+export const signupSchema= joi.object({
+    firstName: joi.string().required(),
+    lastName: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(8).max(150).required(),
+    confirmPassword:joi.string().valid(joi.ref("password")).required(),
+    gender: joi.number().valid(...Object.values(Gender)).required(),
+    phone: joi.string().required(),
+
+})
