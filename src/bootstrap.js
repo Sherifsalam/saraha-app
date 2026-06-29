0bb29c310de8773ed47ff8aad2779eccbdc9d1a8
sherif salama <136256923+Sherifsalam@users.noreply.github.com>
2026-06-29 05:52:50 +0300

    updated version of bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,7 @@
-import express from "express";
+import express from "express"
 import { connectDB } from "./DB/connection.js";
+import router from "./Modules/auth/auth.controller.js"; 
+import cors from "cors"
 // import { signup as usersRouter } from "./Modules/Users/users.route.js";
 // import { notesRouter } from "./Modules/notes/notes.route.js";
 
@@ -7,7 +9,9 @@ export const bootstrap = async () => {
   await connectDB();
   const app = express();
   app.use(express.json());
+  app.use(cors())
 
+  app.use("/api/v1/auth",router)
   // app.use("/users", usersRouter);
   // app.use("/notes", notesRouter);
 
