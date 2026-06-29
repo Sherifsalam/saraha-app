4218749d863dd8be98baca84e8f9a2bf63eef49d
sherif salama <136256923+Sherifsalam@users.noreply.github.com>
2026-06-29 05:51:27 +0300

    updated version of user enums

--- src/DB/enums/user.enums.js
+++ src/DB/enums/user.enums.js
@@ -2,3 +2,18 @@ export const Gender = {
     "male":0,
     "female":1,
 }
+
+
+export const RoleEnum={
+    "user":0,
+    "admin":1,
+    "super_admin":2,
+    "super_super_admin":3,
+    "super_super_super_admin":4
+}
+
+
+export const ProviderEnum={
+    "SYSTEM":0,
+    "GOOGLE":1
+}
