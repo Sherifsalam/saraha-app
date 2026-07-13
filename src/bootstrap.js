e396ae1ccd6063a06f5b59b05bc002a14879c1a2
sherif salama <136256923+Sherifsalam@users.noreply.github.com>
2026-07-14 00:20:18 +0300

    updated version of bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,19 +1,45 @@
-import express from "express"
+import express from "express";
+import multer from "multer";
 import { connectDB } from "./DB/connection.js";
-import router from "./Modules/auth/auth.controller.js"; 
-import cors from "cors"
+import Authrouter from "./Modules/auth/auth.controller.js";
+import usersRouter from "./Modules/Users/users.controller.js";
+import cors from "cors";
+import { redisClient } from "./utils/redis/redis.client.js";
 // import { signup as usersRouter } from "./Modules/Users/users.route.js";
 // import { notesRouter } from "./Modules/notes/notes.route.js";
 
 export const bootstrap = async () => {
   await connectDB();
+
+  redisClient
+    .connect()
+    .then(() => {
+      console.log("Redis connected successfully");
+    })
+    .catch((err) => {
+      console.error("Redis connection error:", err);
+    });
+
+
   const app = express();
   app.use(express.json());
-  app.use(cors())
+  app.use(cors());
 
-  app.use("/api/v1/auth",router)
-  // app.use("/users", usersRouter);
+  app.use("/api/v1/auth", Authrouter);
+  app.use("/api/v1/users", usersRouter);
   // app.use("/notes", notesRouter);
+
+  app.use("/uploads", express.static("./uploads"));
+
+  app.use((err, req, res, next) => {
+    if (err instanceof multer.MulterError) {
+      return res.status(400).json({ message: err.message });
+    }
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
+    next();
+  });
 
   app.listen(process.env.PORT, () => {
     console.log("server running on port 5200");
